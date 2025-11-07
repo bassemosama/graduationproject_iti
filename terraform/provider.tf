bdc5b168bf5ba36adfcc65a87a730eeb09ed2e81
omar <omarriad22@gmail.com>
2025-11-07 16:37:24 +0200

    configured remote backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,12 @@
 provider "aws" {
-  shared_config_files      = ["conf"]
-  shared_credentials_files = ["cred"]
   profile                  = "default"
+}
+terraform {
+  backend "s3" {
+    bucket         = "iti-tf-state-remote-bucket"
+    key            = "./terraform.tfstate" # folder/state file inside the bucket
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
